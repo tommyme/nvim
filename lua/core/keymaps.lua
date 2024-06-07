e4361752452655b10431734af4ef90eaf6cf960b
jerrita <je5r1ta@icloud.com>
2024-06-07 11:11:12 +0800

    feat: fmt on save

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,6 +2,20 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+local format_and_save = function()
+    local params = vim.lsp.util.make_formatting_params({})
+    local bufnr = vim.api.nvim_get_current_buf()
+    local client = vim.lsp.get_active_clients()[1]
+    local handler = function(err, result)
+        if not result then return end
+
+        vim.lsp.util.apply_text_edits(result, bufnr, client.offset_encoding)
+        vim.cmd('write')
+    end
+
+    client.request('textDocument/formatting', params, handler, bufnr)
+end
+
 -- -- insert
 keymap.set("i", "jk", "<ESC>")
 
@@ -14,11 +28,13 @@ keymap.set("v", "K", "<cmd>m '>+2<CR>gv=gv")
 keymap.set("n", "<leader>sv", "<C-w>v")
 keymap.set("n", "<leader>sh", "<C-w>s")
 keymap.set("n", "<leader>qq", "<cmd>q!<CR>")
-keymap.set("n", "<leader>w", "<cmd>w<CR>")
-keymap.set("n", "<leader>wq", "<cmd>wq<CR>")
+keymap.set("n", "<leader>wq", function()
+    format_and_save()
+    vim.cmd('q')
+end)
 keymap.set("n", "<leader>le", vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rrors' })
 keymap.set("n", "<leader>lq", vim.diagnostic.open_float, { desc = 'Show diagnostic [Q]uickfix' })
-
+keymap.set("n", "<leader>w", format_and_save)
 
 -- cancel highlight
 keymap.set("n", "<leader>nh", "<cmd>nohl<CR>")
