c3180b3bf483866dbe68c4d66627d23148067dbd
tommyme-wsl <tommyme@wsl.com>
2024-12-29 23:15:35 +0800

    key for nav back/forward

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+-- navigate back
+map("n", "<A-Left>", "<C-o>", { desc = "Navigate back" })
+map("n", "<A-Right>", "<C-i>", { desc = "Navigate forward" })
